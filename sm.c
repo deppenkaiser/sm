279486f151ddebd0fb95d73d619e9073801fb381
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2025-05-25 10:12:48 +0200

    Microsoft ist Scheiße!

---
+++ sm.c
@@ -0,0 +1,16 @@
+#include "sm/sm.h"
+
+void* state_machine_thread(void* user_data)
+{
+    sm_core_t sm_core = (sm_core_t) user_data;
+    while (sm_core->current_state.state_function(sm_core->user_data, &sm_core->current_state));
+    return NULL;
+}
+
+void sm_run(sm_state_t next_state)
+{
+    struct sm_core sm_core = {0};
+    sm_core.current_state.state_function = next_state->state_function;
+    sm_core.thread = threading_thread_create(state_machine_thread, &sm_core);
+    threading_thread_join(sm_core.thread);
+}
