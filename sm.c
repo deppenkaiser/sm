4f06e7b932eb05c528f4167532e07a7582af9137
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2025-06-11 15:54:34 +0200

    FIX

--- sm.c
+++ sm.c
@@ -11,7 +11,7 @@ void sm_run(sm_state_t next_state, void* user_data)
 {
     struct sm_core sm_core = {0};
     sm_core.current_state.state_function = next_state->state_function;
-    sm_core.thread = threading_thread_create(_sm_thread, &sm_core);
     sm_core.user_data = user_data;
+    sm_core.thread = threading_thread_create(_sm_thread, &sm_core);
     threading_thread_join(sm_core.thread);
 }
